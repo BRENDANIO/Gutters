fce1a350efa1ecf62b9454531aa9737a3fe9d3ec
Brendan Willmott <132240018+BRENDANIO@users.noreply.github.com>
2023-06-18 20:21:44 -0500

    Altered JS function

--- js/MenuDropdown.js
+++ js/MenuDropdown.js
@@ -1,5 +1,16 @@
-function MenuDropdown() {
-    var dropdown = document.querySelector('.dropdown');
-    dropdown.classList.toggle('open');
-}
+let lastHeadingWidth = 0;
+
+function adjustParagraphWidth() {
+    const heading = document.getElementById('text-header');
+    const paragraph = document.getElementById('text-paragraph');
+    const headingWidth = heading.offsetWidth;
+
+    if (headingWidth !== lastHeadingWidth) {
+        paragraph.style.maxWidth = headingWidth + 'px';
+        lastHeadingWidth = headingWidth;
+    }
+  }
   
+  window.addEventListener('resize', adjustParagraphWidth);
+  window.addEventListener('load', adjustParagraphWidth);
+  adjustParagraphWidth(); // Adjust the paragraph width initially
